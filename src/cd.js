25a64e758337fd5b9d094350d72fda6364b8a439
Pandobeer <nikolyav@gmail.com>
2022-12-17 18:39:55 +0100

    refactor: update cd function

--- src/cd.js
+++ src/cd.js
@@ -1,25 +1,8 @@
-import path from 'node:path';
 import { access, constants } from 'node:fs/promises';
-
-const getAbsolutePath = (currentDir, pathToDir) => {
-    //TODO: to delete after checking
-
-    // if (path.isAbsolute(pathToDir)) {
-    // console.log('isAbsolute');
-    // return pathToDir;
-    return path.resolve(currentDir, pathToDir);
-    //TODO: to delete after checking
-
-    // } else {
-    // return path.join(currentDir, pathToDir);
-    // return path.resolve(currentDir, pathToDir);
-
-    // }
-};
+import { getAbsolutePath } from './utils/helpers.js';
 
 export const cd = async (currentDir, pathToDir) => {
     const combinedPath = getAbsolutePath(currentDir, pathToDir);
-
     try {
         await access(combinedPath, constants.F_OK);
         return combinedPath;
