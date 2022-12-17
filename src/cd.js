34f889857e714eb30df5114e138e00c36b0dae99
Pandobeer <nikolyav@gmail.com>
2022-12-17 19:43:09 +0100

    fix: fix cd function

--- src/cd.js
+++ src/cd.js
@@ -1,10 +1,14 @@
-import { access, constants } from 'node:fs/promises';
+import { stat } from 'node:fs/promises';
 import { getAbsolutePath } from './utils/helpers.js';
 
 export const cd = async (currentDir, pathToDir) => {
     const combinedPath = getAbsolutePath(currentDir, pathToDir);
     try {
-        await access(combinedPath, constants.F_OK);
+        const stats = await stat(combinedPath);
+
+        if (stats.isFile()) {
+            throw Error('not a directory');
+        }
         return combinedPath;
     } catch (err) {
         console.error(`Operation failed: ${err}`);
