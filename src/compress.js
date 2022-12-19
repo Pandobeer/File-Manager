89c2d3fd05465d8eebf7ae345cb79b73839e3061
Pandobeer <nikolyav@gmail.com>
2022-12-19 21:23:11 +0100

    fix: fix compress, decompress functions

--- src/compress.js
+++ src/compress.js
@@ -1,7 +1,9 @@
 import zlib from 'node:zlib';
-import { pipeline } from 'node:stream/promises';
+import { pipeline } from 'stream';
 import fs from 'node:fs';
 import { basename } from 'node:path';
+import { stat } from 'node:fs/promises';
+
 
 import { getAbsolutePath } from './utils/helpers.js';
 
@@ -11,13 +13,38 @@ export const compress = async (currentDir, pathToFile, pathToDestDir) => {
 
     const fileToCompressPath = getAbsolutePath(currentDir, pathToFile);
     const fileCompressedPath = getAbsolutePath(currentDir, pathToDestDir, compressedFile);
+    const dirCompressedPath = getAbsolutePath(currentDir, pathToDestDir);
 
-    const brotli = zlib.createBrotliCompress();
-    const readStream = fs.createReadStream(fileToCompressPath);
-    const writeStream = fs.createWriteStream(fileCompressedPath, { flags: 'wx' });
+    try {
+        await stat(fileToCompressPath).then((file) => {
+            if (!file.isFile()) {
+                throw Error();
+            }
+        }).catch(() => {
+            throw Error(`${fileToCompressPath} is not a file or does not exist.`);
+        });
 
-    await pipeline(readStream, brotli, writeStream).catch((err) => {
+        await stat(dirCompressedPath).then((dir) => {
+            if (!dir.isDirectory()) {
+                throw Error();
+            }
+        }).catch(() => {
+            throw Error(`${dirCompressedPath} is not a directory or does not exist.`);
+        });
+
+        const brotli = zlib.createBrotliCompress();
+        const readStream = fs.createReadStream(fileToCompressPath);
+        const writeStream = fs.createWriteStream(fileCompressedPath, { flags: 'wx' });
+
+        pipeline(readStream, brotli, writeStream, (err) => {
+            if (err) {
+                console.error(`Operation failed: ${err.message}`);
+                process.exitCode = 1;
+            } else {
+                console.log(`${filename} is compressed!`);
+            }
+        });
+    } catch (err) {
         console.error(`Operation failed: ${err.message}`);
-        process.exitCode = 1;
-    });
+    }
 };
